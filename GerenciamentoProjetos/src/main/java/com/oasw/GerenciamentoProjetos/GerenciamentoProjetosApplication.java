a599f39e3bed8ffaa6a25b8f8562b925b15691a6
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 19:29:34 -0300

    model criado

---
+++ GerenciamentoProjetos/src/main/java/com/oasw/GerenciamentoProjetos/GerenciamentoProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.oasw.GerenciamentoProjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoProjetosApplication.class, args);
+	}
+
+}
